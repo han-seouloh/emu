1ec27539c73ec16751c823823f5928ef1edfb294
AlonsoBarajas <barjasalonsoc@gmail.com>
2022-11-25 12:51:20 -0600

    .gitignore

---
+++ build/CMakeFiles/emu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emu.dir/main.cpp.o"
+  "CMakeFiles/emu.dir/main.cpp.o.d"
+  "emu"
+  "emu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/emu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
